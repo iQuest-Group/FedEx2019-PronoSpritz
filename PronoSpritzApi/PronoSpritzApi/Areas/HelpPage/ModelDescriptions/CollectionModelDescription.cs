849b0c36bbc9a8d19fbf83477e0f139ca36e8e00
daniel.savu <Daniel.Savu@iquestgroup.com>
2019-11-22 09:45:20 +0200

    Added PronospritzApi backend

---
+++ PronoSpritzApi/PronoSpritzApi/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace PronoSpritzApi.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
