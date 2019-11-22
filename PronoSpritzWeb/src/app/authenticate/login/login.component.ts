9d65004a16ce4b3f2ea6db6a8c0958373aec46d1
Sebastian Serban <sebastian.serban@iquestgroup.com>
2019-11-22 16:39:15 +0200

    added dashboard draft

--- PronoSpritzWeb/src/app/authenticate/login/login.component.ts
+++ PronoSpritzWeb/src/app/authenticate/login/login.component.ts
@@ -31,6 +31,8 @@ export class LoginComponent implements OnInit {
     this.userService.login(email, password).subscribe(() => {
       this.router.navigate(['login']);
     });
+
+    localStorage.setItem('access_token', 'asdasdsad');
   }
 
   goToRegister()
